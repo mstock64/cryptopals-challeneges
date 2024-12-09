bc8411cc1031c6e23efc7a3b8baa7aa994933298
max <stockm0819@gmail.com>
2024-12-09 14:31:13 -0500

    Solved Challenge 2 and 3; Added Unit Tests

--- set1/challenges.go
+++ set1/challenges.go
@@ -19,3 +19,47 @@ func HexToBase64(payload []byte) []byte {
 	}
 	return []byte(base64.RawURLEncoding.EncodeToString(plainText))
 }
+
+/*
+Challenge 2: Fixed XOR
+Pre: Byte Arrays are the same length
+Post
+*/
+func FixedXor(left, right []byte) []byte {
+	leng := len(left)
+	result := make([]byte, leng)
+
+	for i := 0; i < leng; i++ {
+		result[i] = left[i] ^ right[i]
+	}
+
+	return result
+}
+
+/*
+Challenge 2: Single Byte Xor
+Pre:
+Post
+*/
+func SingleByteXor(data []byte, single byte) []byte {
+	leng := len(data)
+	result := make([]byte, leng)
+
+	for i := 0; i < leng; i++ {
+		result[i] = data[i] ^ single
+	}
+
+	return result
+}
+
+func numberOfValidChar(data []byte) int {
+	count := 0
+	for _, entry := range data {
+		if entry > 63 && entry < 91 {
+			count++
+		} else if entry > 96 && entry < 123 {
+			count++
+		}
+	}
+	return count
+}
--- set1/challenges_test.go
+++ set1/challenges_test.go
@@ -1,6 +1,10 @@
 package set1
 
-import "testing"
+import (
+	"encoding/hex"
+	"log"
+	"testing"
+)
 
 func TestHexToBase64(T *testing.T) {
 	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -8,6 +12,41 @@ func TestHexToBase64(T *testing.T) {
 	result := HexToBase64([]byte(input))
 
 	if string(result) != expectedOutput {
+		T.FailNow()
+	}
+}
+
+func TestFixedXOR(T *testing.T) {
+	left, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	right, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	expectedOutput := "746865206b696420646f6e277420706c6179"
+	result := FixedXor(left, right)
+
+	if hex.EncodeToString(result) != expectedOutput {
+		log.Println(string(result))
+		T.FailNow()
+	}
+}
+
+func TestSingeByteXOR(T *testing.T) {
+	data, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+
+	expectedOutput := "Cooking MC's like a pound of bacon"
+	var results []int
+	for i := 0; i < 127; i++ {
+		entry := SingleByteXor(data, byte(i))
+		num := numberOfValidChar(entry)
+		results = append(results, num)
+	}
+	best, index := 0, 0
+	for i := 0; i < len(results); i++ {
+		if best < results[i] {
+			best = results[i]
+			index = i
+		}
+	}
+	gamble := SingleByteXor(data, byte(index))
+	if string(gamble) != expectedOutput {
 		T.FailNow()
 	}
 }
